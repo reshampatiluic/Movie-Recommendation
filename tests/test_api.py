c5b09a2e95b23e525217b56586b9defeb8f3ebd5
Sohan <sohannaidu123@gmail.com>
2025-04-13 03:55:32 -0500

    update requirements and fix imports

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,10 +1,4 @@
 from fastapi.testclient import TestClient
-import os
-import sys
-
-current = os.path.dirname(os.path.realpath(__file__))
-parent = os.path.dirname(current)
-sys.path.append(parent)
 from app.server import app
 
 client = TestClient(app)
